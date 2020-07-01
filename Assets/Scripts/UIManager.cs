2e96e3cdd51399894072cbf572caeee40b1a4f48
ebasile96 <edoardo.basile96@gmail.com>
2020-07-01 16:19:17 +0200

    added buttons, changed UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button Desc, View;
+    public GameObject DescObj, ViewObj;
+
+    private void Start()
+    {
+        Desc.interactable = false;
+        DescObj.SetActive(true);
+    }
+    public void ToggleToView()
+    {
+        Desc.interactable = true;
+        View.interactable = false;
+        DescObj.SetActive(false);
+        ViewObj.SetActive(true);
+    }
+
+    public void ToggleToDesc()
+    {
+        View.interactable = true;
+        Desc.interactable = false;
+        ViewObj.SetActive(false);
+        DescObj.SetActive(true);
+
+    }
+}
