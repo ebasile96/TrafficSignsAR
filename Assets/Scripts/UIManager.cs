d905f787da2178759cc1e6c684917e0646c7a34d
ebasile96 <edoardo.basile96@gmail.com>
2020-07-01 17:48:03 +0200

    modificato ui manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,28 +5,76 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public List<GameObject> descriptions = new List<GameObject>();
+    public List<GameObject> views = new List<GameObject>();
     public Button Desc, View;
-    public GameObject DescObj, ViewObj;
 
     private void Start()
     {
         Desc.interactable = false;
-        DescObj.SetActive(true);
+        Setup();
+        SetDescOn();
     }
     public void ToggleToView()
     {
         Desc.interactable = true;
         View.interactable = false;
-        DescObj.SetActive(false);
-        ViewObj.SetActive(true);
+        SetViewsOn();
     }
 
     public void ToggleToDesc()
     {
         View.interactable = true;
         Desc.interactable = false;
-        ViewObj.SetActive(false);
-        DescObj.SetActive(true);
+        SetDescOn();
+    }
 
+    public void Setup()
+    {
+        //findDescs();
+        //findViews();
+        Desc.interactable = false;
+    }
+
+    public void findDescs()
+    {
+        foreach (GameObject _desc in GameObject.FindGameObjectsWithTag("Description"))
+        {
+            descriptions.Add(_desc);
+        }
+    }
+
+    public void findViews()
+    {
+        foreach (GameObject _views in GameObject.FindGameObjectsWithTag("3DView"))
+        {
+            descriptions.Add(_views);
+        }
+    }
+
+    public void SetDescOn()
+    {
+        foreach (GameObject view in views)
+        {
+            view.SetActive(false);
+        }
+
+        foreach (GameObject desc in descriptions)
+        {
+            desc.SetActive(true);
+        }
+    }
+
+    public void SetViewsOn()
+    {
+        foreach (GameObject desc in descriptions)
+        {
+            desc.SetActive(false);
+        }
+
+        foreach (GameObject view in views)
+        {
+            view.SetActive(true);
+        }
     }
 }
